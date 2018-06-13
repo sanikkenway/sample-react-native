900dd9633acc75e2fc80d974aa4a627c9cbf6117
Nik Saiful Anuar <sanik90@gmail.com>
2018-06-13 14:32:12 +0800

    add header

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// import library
+// import App from './App';
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+// component
+const App = () => (
+    <Header headerText={'Album'} />
+);
+
+// render to device
 AppRegistry.registerComponent('mobileapp', () => App);
