c94764b1e0fcd36245f7243347618bac5d5ca13b
Nik Saiful Anuar <sanik90@gmail.com>
2018-06-22 17:17:33 +0800

    add firebase auth and spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: <>,
+      authDomain: <>,
+      databaseURL: <>,
+      projectId: <>,
+      storageBucket: <>,
+      messagingSenderId: <>
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Logout
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />
+    }
+  }
+
   render() {
     return (
       <View>
-      <Header headerText='Authentication' />
-        <LoginForm />
+        <Header headerText='Authentication' />
+        {this.renderContent()}
       </View>
     );
   }
