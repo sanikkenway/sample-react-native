2c37b01312a1f6e0354d90a3efd41ad607045b81
Nik Saiful Anuar <sanik90@gmail.com>
2018-06-26 11:44:16 +0800

    rename file, hnghhh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import library
 import { AppRegistry } from 'react-native';
-import App from './src/App';
+import App from './src/app';
 
 // render to device
 AppRegistry.registerComponent('mobileapp', () => App);
